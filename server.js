27e34842f9db19e0a0a44a8bf8b0f0b91f8c9ed0
Trasom Zeng <trasom.zeng@ericsson.com>
2017-08-24 19:25:07 +0800

    Bug#19832, update server
    Change-Id: I69699aa29d9846713880f70b90c0957337818bd7

--- server.js
+++ server.js
@@ -8,9 +8,10 @@ new WebpackDevServer(webpack(config), { // Start a server
   publicPath: config.output.publicPath,
   hot: true, // With hot reloading
   inline: false,
+  disableHostCheck: true,
   historyApiFallback: true,
   quiet: false // Without logging
-}).listen(3003, 'localhost', function (err, result) {
+}).listen(3003, '0.0.0.', function (err, result) {
   if (err) {
     console.log(err);
   }
