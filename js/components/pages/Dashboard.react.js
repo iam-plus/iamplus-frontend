0df8bbc0f60436410a373c369c54b7f860bc3501
Kainian Yin <kainian.yin@ericsson.com>
2017-08-24 18:24:36 +0800

    Others#0, fix.
    Change-Id: I659f7e5934e8dce0e1a128d879eb433b715879d4

--- js/components/pages/Dashboard.react.js
+++ js/components/pages/Dashboard.react.js
@@ -8,7 +8,7 @@
 
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Bar as BarChart, Line as LineChart } from "react-chartjs";
+import {Bar as BarChart, Line as LineChart} from 'react-chartjs-2';
 import R from 'ramda';
 
 class Dashboard extends Component {
@@ -23,16 +23,18 @@ class Dashboard extends Component {
     const validateSet = this.getValidateSet();
 
     const barChartData = {
-      labels: R.range(1, 15),
+      labels: R.range(0, 15),
       datasets: [{
-        label: 'Dataset',
-        fillColor: 'rgb(153, 50, 204)',
-        data: [20, 98, 60]
+        label: "test 1",
+        backgroundColor: ['rgb(153, 50, 204)', 'rgb(255, 0, 0)', 'rgb(0, 255, 0)'],
+        data: [25, 30, 100]
       }]
     };
 
     const barChartOptions = {
-      tooltipTemplate: "<%= value %>",
+      tooltip: {
+        enabled: false,
+      }
     }
 
     const datasets = R.addIndex(R.map)((data, idx) => ({ label: idx, data }), validateSet || []);
@@ -50,9 +52,12 @@ class Dashboard extends Component {
     };
 
     const detailChartOptions = {
-      bezierCurve: false,
-      datasetFill : false,
-      tooltipTemplate: "<%if (label){%><%=label%>: <%}%><%= value %>",
+      elements: {
+            line: {
+                tension: 0, // disables bezier curves
+            }
+        },
+      fill : false,
     };
     
     return (
