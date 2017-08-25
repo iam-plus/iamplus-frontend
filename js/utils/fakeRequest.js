3d6c44a47756e76ee77c94ec80cad83f3f6c8778
Kainian Yin <kainian.yin@ericsson.com>
2017-08-25 10:45:23 +0800

    Others#, remove timeout.
    Change-Id: I32e47c9ce482bde89829309abd6a6c26fc0b1d7e

--- js/utils/fakeRequest.js
+++ js/utils/fakeRequest.js
@@ -12,23 +12,20 @@ var fakeRequest = {
    * @param  {?function} callback Called after the server successfully did it's thing
    */
   post(endpoint, data, callback) {
-    // Delay the call by a random amount between 100ms and 2000ms
     // to simulate network latency
-    setTimeout(() => {
-      switch (endpoint) {
-        case '/login':
-          server.login(data.username, data.password, callback);
-          break;
-        case '/register':
-          server.register(data.username, data.password, callback);
-          break;
-        case '/logout':
-          server.logout(callback);
-          break;
-        default:
-          break;
+    switch (endpoint) {
+      case '/login':
+        server.login(data.username, data.password, callback);
+        break;
+      case '/register':
+        server.register(data.username, data.password, callback);
+        break;
+      case '/logout':
+        server.logout(callback);
+        break;
+      default:
+        break;
       }
-    }, (Math.random() * 2000) + 100);
   }
 }
 
